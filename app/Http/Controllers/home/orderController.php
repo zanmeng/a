e1945177e24431d38e8b882d47520757e0d65999
zanmeng <1916150942@qq.com>
2018-04-04 15:20:20 +0800

    dingdan zm

--- app/Http/Controllers/home/orderController.php
+++ app/Http/Controllers/home/orderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\home;
 
+use App\Model\homeUser;
 use App\Model\orderGoods;
 use App\Model\orders;
 use Illuminate\Http\Request;
@@ -21,24 +22,75 @@ class orderController extends Controller
     // 生成订单
     public function index()
     {
-        // 判断当前用户是否登录了
-//        $userName = session()->get('user')->userName;
-//        dd($user);
-        $orderNum = date('YmdHis').mt_rand(1000,9999);
+        $login_id = session()->get('user')->login_id;
+        $userName= session()->get('user')->userName;
+//        dd($login_id);
+
+        //拿购物车信息
+       $carts= DB::table('carts')
+            ->leftJoin('goods','carts.gid','=','goods.gid')
+            ->leftJoin('good_color','carts.colorId','=','good_color.colorId')
+            ->leftJoin('good_version','carts.vid','=','good_version.vid')
+           ->where('carts.login_id','=',$login_id)
+            ->get();
+//        dd($carts);
+        //拿地址信息
+        $address = DB::table('address')->where('login_id',$login_id)->where('status','1')->get();
+//        $address = DB::select('select * from address where login_id = :id and status = :status' ,['id' => $login_id,'status' => 1]);
+//        dd($address);
+        if(!empty($address)){
+           echo  23;
+        }
+        die;
+        foreach ($address as $v ) {
+
+        }
+        // 求总数量和总金额
+        $orderCnt = 0;  // 总数量
+        $orderMoney = 0;  // 总金额
+
+        foreach ($carts as $v){
+//           $goodsPic =$v->gpic;
+//           $gname = $v->gname;
+//           $version = $v->version;
+//           $goodsColor = $v->color;
+//           $goodNum = $v->num;
+//           $goodsPrice =$v->price;
+           $orderMoney +=($v->price)*($v->num);
+           $orderCnt += $v->num;
+
+        }
+//        dd($orderMoney);
+//        dd($goodNum);
+
+        $orderNum = date('YmdHis').mt_rand(10000,99999);
         $time = date('Y-m-d H:i:s');
 //       dd($time) ;
 //        if(!empty(session('message')) && !empty(session('url')) && !empty(session('jumpTime'))){
+            //添加到订单表
            $res= orders::create(['orderNum'=>$orderNum,
-                                'userName'=>232,
+                               'userName'=>$userName,
                                 'createTime'=>$time,
-
+                                'login_id'=>$login_id,
+                                'orderMoney'=>$orderMoney,           //总价
+                                'orderCnt'=>$orderCnt,              //总数
+                                //地址相关字段   刷新   图片 问题
+               //               'orderName'=>$gname,
 
                ]);
+           //添加到订单表详情表
+//        dd($carts);
+        foreach ($carts as $v ){
            $res2 = orderGoods::create(['orderNum'=>$orderNum,
-
-
+                                        'goodsName'=>$v->gname,
+                                        'goodsVersion'=>$v->version,
+                                        'goodsColor'=>$v->color,
+                                        'goodsNum'=>$v->num,
+                                        'goodsPrice'=>$v->price,
+                                        'goodsPic'=>$v->gpic,
 
                ]);
+        }
 //            dd($res2);
 //        DB::beginTransaction();
 //        try{
@@ -59,7 +111,7 @@ class orderController extends Controller
 
 
         //显示模板及数据
-        return view('home.order.index');
+        return view('home.order.index',compact('carts','orderNum'));
 
 
 
--- resources/views/home/order/index.blade.php
+++ resources/views/home/order/index.blade.php
@@ -4,7 +4,7 @@
 	<!-- end banner_x -->
 	<!-- self_info -->
     <style>
-        #a li{float: left;font-size: 15px;width: 250px;height:20px; margin-left: 40px;}
+        #a li{float: left;font-size: 15px;width: 210px;height:20px; margin-left: 40px;color: #aaa;padding: 10px;}
         /*#a1{width: 150px;height:20px; margin-left: 0px;border:1px blue dashed;}*/
     </style>
 
@@ -32,88 +32,64 @@
 			{{--</div>--}}
 			<div class="rtcont fr">
 				<div class="ddzxbt">确认订单 </div>
-                <div style="display:block;height:40px">
-                    <ul id="a">
-                        <li id="a1">姓名：士大夫似的</li>
-                        <li id="a2">送货地址：豆腐格的风格风格地的人感到反感的风格的风格方豆腐干地方</li>
-                        <li id="a3">电话：18064261826</li>
-                    </ul>
-                </div>
-
+                {{--<div style="display:block;height:40px;margin-top: 10px " >--}}
+                    {{--<ul id="a">--}}
+                        {{--<li id="a1">订单号：2018040301191722191</li>--}}
+                        {{--<li id="a2">收货人：士大夫似的</li>--}}
+                        {{--<li id="a3">收货人电话：18064261826</li>--}}
+                    {{--</ul>--}}
+                {{--</div>--}}
+                {{--<div style="color: #aaa;height: 26px; margin-left: 41px">送货地址：豆腐格的风格风格地的人感到反感的风格的风格方豆腐干地方</div>--}}
+                @foreach($carts as $v)
+                    {{--style="height: 110px;width:80px"style="float: left;height: 110px;width:80px"--}}
 				<div class="ddxq" >
-					<div class="ddspt fl"><img src="./image/gwc_xiaomi6.jpg" alt=""></div>
-					<div class="ddbh fl"  >商品名鬼地方鬼地方感到翻跟斗翻跟斗</div>
+					<div class="ddspt fl" ><img src="{{$v->gpic}}" alt="" style="height: 110px;width:80px;padding-top: 0px"></div>
+					<div class="ddbh fl"   >{{$v->gname}}</div>
 					<div class="ztxx fr">
 						<ul >
-							<li>6个叫24该楼上的疯了</li>
-							<li>规格士大夫随</li>
-							<li>X数量</li>
-							<li>￥2499.00</li>
+							<li>{{$v->version}}</li>
+							<li>{{$v->color}}</li>
+							<li>{{$v->num}}</li>
+							<li>{{($v->price)*($v->num)}}</li>
 							{{--<li><a href="">订单详情></a></li>--}}
 							<div class="clear"></div>
 						</ul>
 					</div>
 					<div class="clear"></div>
 				</div>
-
-                <div class="ddxq">
-                    <div class="ddspt fl"><img src="./image/gwc_xiaomi6.jpg" alt=""></div>
-                    <div class="ddbh fl">商品名</div>
-                    <div class="ztxx fr">
-                        <ul>
-                            <li>规格</li>
-                            <li>X数量</li>
-                            <li>￥2499.00</li>
-                            {{--<li><a href="">订单详情></a></li>--}}
-                            <div class="clear"></div>
-                        </ul>
-                    </div>
-                    <div class="clear"></div>
-                </div>
-				<div class="ddxq">
-					<div class="ddspt fl"><img src="./image/liebiao_hongmin4_dd.jpg" alt=""></div>
-					<div class="ddbh fl">订单号:170526435444865</div>
-					<div class="ztxx fr">
-						<ul>
-							<li>已发货</li>
-							<li>￥1999.00</li>
-							<li>2017/05/26 14:02</li>
-							{{--<li><a href="">订单详情></a></li>--}}
-							<div class="clear"></div>
-						</ul>
-					</div>
-					<div class="clear"></div>
-				</div>
-
-
-
-
-
-
-
-
-
-
-
+                @endforeach
 
 
                 <div class="jiesuandan mt20 center">
-                    <div class="tishi fl ml20">
-                        <ul>
-                            <li><a href="./liebiao.html">继续购物</a></li>
-                            <li>|</li>
-                            <li>共<span>2</span>件商品，已选择<span>1</span>件</li>
-                            <div class="clear"></div>
-                        </ul>
+                    {{--<div class="tishi fl ml20">--}}
+                        {{--<ul>--}}
+                            {{--<li><a href="./liebiao.html">继续购物</a></li>--}}
+                            {{--<li>|</li>--}}
+                            {{--<li>共<span>2</span>件商品，已选择<span>1</span>件</li>--}}
+                            {{--<div class="clear"></div>--}}
+                        {{--</ul>--}}
+                    {{--</div>--}}
+                    <div  style=" border:1px #01AAED solid; float: right;width: 400px; height: 200px">
+                        <div style="display:block;height:40px" >
+                            <ul id="a">
+                                <li id="a1">订单号：{{$orderNum}}</li>
+                                <li id="a2">收货人：士大夫似的</li>
+                                <li id="a3">收货人电话：18064261826</li>
+                            </ul>
+                            <div style="color: #aaa;margin-left: 50px;float: left;font-size: 15px;">送货地址：豆腐格的风格风格地的人感到反感的风格的风格方豆腐干地方</div>
+                        </div>
+                        {{--<div style="color: #aaa;margin-left: 41px">送货地址：豆腐格的风格风格地的人感到反感的风格的风格方豆腐干地方</div>--}}
+
                     </div>
                     <div class="jiesuan fr">
+
                         <div class="jiesuanjiage fl">合计（不含运费）：<span>2499.00元</span></div>
 {{--                        {{$user = session()->get('user')}}--}}
-                        @if (!(session()->get('user')))
-                            <div class="jsanniu fr"><input class="jsan" type="text" name="jiesuan"  value=" &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;请登陆"/></div>
-                        @else
+                        {{--@if (!(session()->get('user')))--}}
+                            {{--<div class="jsanniu fr"><a href="/home/login"><input class="jsan" type="text" name="jiesuan"  value=" &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;请登陆"/></div></a>--}}
+                        {{--@else--}}
                             <div class="jsanniu fr"><input class="jsan" type="submit" name="jiesuan"  value="提交订单"/></div>
-                        @endif
+                        {{--@endif--}}
 
                         <div class="clear"></div>
                     </div>
